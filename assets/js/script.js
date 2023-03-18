b0dac211ef307aca8595f1ace84bc3da59797bff
VANDA Lab <91102057+VANDAlab@users.noreply.github.com>
2023-03-18 17:28:55 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,19 @@
 
     setInterval(changeImage, interval);
 })();
+
+
+function openTab(evt, tabId) {
+  const tabs = document.getElementsByClassName("tab");
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+  }
+
+  const tabButtons = document.getElementsByClassName("tab-btn");
+  for (let i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].classList.remove("active");
+  }
+
+  document.getElementById(tabId).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
