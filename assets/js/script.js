4bb00c1c17aa81a762d93a4f7990b39cf9d317fc
VANDA Lab <91102057+VANDAlab@users.noreply.github.com>
2023-03-18 13:39:39 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
- 
+(function() {
+    var slider = document.getElementById('image-slider');
+    var images = slider.getElementsByTagName('img');
+    var currentIndex = 0;
+    var interval = 3000; // Duration in milliseconds between image changes
+
+    function changeImage() {
+        images[currentIndex].classList.remove('visible');
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add('visible');
+    }
+
+    setInterval(changeImage, interval);
+})();
