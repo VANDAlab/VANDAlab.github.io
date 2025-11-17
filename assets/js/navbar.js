5a7e3239bc628583e4b0906d95b100b6059710d4
VANDA Lab <91102057+VANDAlab@users.noreply.github.com>
2025-11-16 22:42:36 -0500

    Create navbar.js

---
+++ assets/js/navbar.js
@@ -0,0 +1,9 @@
+document.write(`
+    <nav class="navbar">
+         <a href="index.html">Home</a>
+      <a href="research.html">Research</a>
+      <a href="team.html">Team</a>
+      <a href="publications.html">Publications</a>
+      <a href="resources.html">Resources</a>
+    </nav>
+    `);
